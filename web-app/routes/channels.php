314591bcc3e1482341409126b58e3f961d7b5a0a
204309 <nainariffin@gmail.com>
2025-11-23 23:58:17 +0800

    Disabling broadcast channel temporarily

--- web-app/routes/channels.php
+++ web-app/routes/channels.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Broadcast;
+// use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('file-process-status-updates', function () {
-    return true;
-});
+// Broadcast::channel('file-process-status-updates', function () {
+//     return true;
+// });
